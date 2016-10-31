11a57b08059601debb647ca9334783b2752b967f
jalenkjx <jinlei569@gmail.com>
2016-10-31 22:02:29 +0800

    80端口

--- servers/app.js
+++ servers/app.js
@@ -5,6 +5,6 @@ var app = new express();
 var staticpath = path.join(__dirname, "../static");
 
 app.use (express.static(staticpath));
-app.listen(3000,function(){
-	console.log('http://127.0.0.1:3000')
+app.listen(80,function(){
+	console.log('http://127.0.0.1:80')
 })
