db80aad463cb13969efb716a3d209b570fa58186
jalenkjx <jinlei569@gmail.com>
2016-11-01 21:30:14 +0800

    modify

--- servers/app.js
+++ servers/app.js
@@ -4,6 +4,9 @@ var app = new express();
 
 var path = require('path');
 
+//加载 token 模块
+require("./token")(app);
+
 var staticpath = path.join(__dirname, "../static");
 
 app.use (express.static(staticpath));
---
+++ servers/config.js
@@ -0,0 +1,15 @@
+/**
+ * 微信认证后的 app 信息
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的数据
+ */
+module.exports = function(){
+	return {
+		//AppID(应用ID)
+		appid: 'wx3ae6e7bd7a2acbb4',
+		//AppSecret(应用密钥)
+		appsecret: '4a8bf64bff23e7c399b9ee0805593440',
+		//Token(令牌)
+		token: 'wx_jinlei'
+	};
+};
